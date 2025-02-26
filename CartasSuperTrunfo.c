56b3056903e526013326d2f4277a8d1e88c08845
DevPedro-code <pedroadeall@gmail.com>
2025-02-26 21:02:43 +0000

    programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,21 +3,23 @@
 int main(){
 
     char Estado[20];
-    int Codigo;
+    char Codigo[20];
     char Cidade[20];
     int Populacao;
     float AreaKm;
     float PIB;
     int PontoTuristicos;
 
+    printf("carta 1 \n");
+
 
     printf("Nome do Estado: \n");
     scanf("%s", &Estado);
 
     printf("Digite seu Codigo: \n");
-    scanf("%d", &Codigo);
+    scanf("%s", &Codigo);
 
-    printf("Nome da Cidade: %s\n", Cidade);
+    printf("Nome da Cidade: \n");
     scanf("%s", &Cidade);
 
     printf("Digite a População: \n");
@@ -33,11 +35,44 @@ int main(){
     scanf("%d", &PontoTuristicos);
 
     printf("Estado: %s\n", Estado);
-    printf("Codigo: %d\n", Codigo);
+    printf("Codigo: %s\n", Codigo);
     printf("Cidade: %s\n", Cidade);
     printf("Populacao: %d\n", Populacao);
     printf("AreaKm: %f\n", AreaKm);
     printf("PIB: %f\n", PIB);
     printf("Pontos Turisticos: %d\n", PontoTuristicos);
 
+
+    printf("carta 2 \n");
+
+
+    printf("Nome do Estado: \n");
+    scanf("%s", &Estado);
+
+    printf("Digite seu Codigo: \n");
+    scanf("%s", &Codigo);
+
+    printf("Nome da Cidade: \n");
+    scanf("%s", &Cidade);
+
+    printf("Digite a População: \n");
+    scanf("%d", &Populacao);
+
+    printf("Digite a AreaKm: \n");
+    scanf("%f", &AreaKm);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Ponto Turisticos: \n");
+    scanf("%d", &PontoTuristicos);
+
+    printf("Estado: %s\n", Estado);
+    printf("Codigo: %s\n", Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Populacao: %d\n", Populacao);
+    printf("AreaKm: %f\n", AreaKm);
+    printf("PIB: %f\n", PIB);
+    printf("Pontos Turisticos: %d\n", PontoTuristicos);
+    
 }
